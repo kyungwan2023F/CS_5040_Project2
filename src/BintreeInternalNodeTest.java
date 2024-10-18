59943194eeeebaf52d1038449794969267df924c
kyungwan2023F <kyungwan@vt.edu>
2024-10-18 17:12:50 -0400

    ..
    .

--- src/BintreeInternalNodeTest.java
+++ src/BintreeInternalNodeTest.java
@@ -62,8 +62,7 @@ public class BintreeInternalNodeTest extends TestCase {
     public void testInsertDuplicateLocation() {
         BintreeNode result = internalNode.insert(5, 5, 0, 0, 10, 10, seminar1,
             0);
-        result = internalNode.insert(5, 5, 0, 0, 10, 10, seminar2, 0); // Same
-                                                                       // location
+        result = internalNode.insert(seminar1.x(), seminar1.y(), 0, 0, 10, 10, seminar1, 0);
         assertTrue(result instanceof BintreeInternalNode);
     }
 
@@ -88,9 +87,9 @@ public class BintreeInternalNodeTest extends TestCase {
 
     public void testInsertIntoMultipleDepths() {
         internalNode.insert(5, 5, 0, 0, 10, 10, seminar1, 0);
-        internalNode.insert(3, 3, 0, 0, 10, 10, seminar2, 0);
-        internalNode.insert(8, 8, 0, 0, 10, 10, seminar3, 0);
-        internalNode.insert(6, 7, 0, 0, 10, 10, seminar4, 0);
+        internalNode.insert(seminar2.x(), seminar2.y(), 0, 0, 10, 10, seminar2, 0);
+        internalNode.insert(seminar3.x(), seminar3.y(), 0, 0, 10, 10, seminar3, 0);
+        internalNode.insert(seminar4.x(), seminar4.x(), 0, 0, 10, 10, seminar4, 0);
 
         assertTrue(internalNode.left instanceof BintreeLeafNode);
         assertTrue(internalNode.right instanceof BintreeInternalNode);
@@ -98,8 +97,8 @@ public class BintreeInternalNodeTest extends TestCase {
 
 
     public void testDeleteWithMergeUp() {
-        internalNode.insert(5, 5, 0, 0, 20, 20, seminar1, 0);
-        internalNode.insert(8, 8, 0, 0, 20, 20, seminar3, 0);
+        internalNode.insert(seminar1.x(), seminar1.y(), 0, 0, 20, 20, seminar1, 0);
+        internalNode.insert(seminar3.x(), seminar3.y(), 0, 0, 20, 20, seminar3, 0);
 
         BintreeNode result = internalNode.delete(5, 5, 1, 0, 0, 20, 20, 0);
         assertTrue(result instanceof BintreeLeafNode);
@@ -119,8 +118,8 @@ public class BintreeInternalNodeTest extends TestCase {
 
 
     public void testDeleteNonExistentNode() {
-        internalNode.insert(5, 5, 0, 0, 10, 10, seminar1, 0);
-        internalNode.insert(3, 3, 0, 0, 10, 10, seminar2, 0);
+        internalNode.insert(seminar1.x(), seminar1.y(), 0, 0, 10, 10, seminar1, 0);
+        internalNode.insert(seminar2.x(), seminar2.y(), 0, 0, 10, 10, seminar2, 0);
         BintreeNode result = internalNode.delete(5, 5, 999, 0, 0, 10, 10, 0);
         int nodesVisited = result.search(0, 0, 0, 0, 10, 10, 20, 20, 0);
         assertEquals(3, nodesVisited);
@@ -128,43 +127,18 @@ public class BintreeInternalNodeTest extends TestCase {
 
 
     public void testSearchSuccess() {
-        internalNode.insert(9, 9, 0, 0, 10, 10, seminar1, 0);
-        internalNode.insert(8, 8, 0, 0, 10, 10, seminar2, 0);
+        internalNode.insert(seminar1.x(), seminar1.y(), 0, 0, 10, 10, seminar1, 0);
+        internalNode.insert(seminar2.x(), seminar2.y(), 0, 0, 10, 10, seminar2, 0);
         int nodesVisited = internalNode.search(8, 8, 0, 0, 10, 10, 1, 1, 0);
         assertEquals(2, nodesVisited);
     }
 
 
     public void testSearchFailure() {
-        internalNode.insert(3, 3, 0, 0, 10, 10, seminar1, 0);
-        internalNode.insert(8, 8, 0, 0, 10, 10, seminar2, 0);
+        internalNode.insert(seminar1.x(), seminar1.y(), 0, 0, 10, 10, seminar1, 0);
+        internalNode.insert(seminar2.x(), seminar2.y(), 0, 0, 10, 10, seminar2, 0);
         int nodesVisited = internalNode.search(9, 9, 0, 0, 10, 10, 1, 1, 0);
         assertEquals(2, nodesVisited);
-    }
-
-
-    public void testSearchAtBoundary() {
-        Seminar seminar5 = new Seminar(5, "Seminar 1", "2024-10-20", 60,
-            (short)1, (short)0, 100, new String[] { "tech" }, "First Seminar");
-        Seminar seminar6 = new Seminar(6, "Seminar 2", "2024-11-20", 60,
-            (short)1, (short)2, 200, new String[] { "science" },
-            "Second Seminar");
-        Seminar seminar7 = new Seminar(7, "Seminar 3", "2024-12-20", 60,
-            (short)2, (short)1, 300, new String[] { "math" }, "Third Seminar");
-        internalNode.insert(1, 0, 0, 0, 4, 4, seminar5, 0);
-        internalNode.insert(1, 2, 0, 0, 4, 4, seminar6, 0);
-        internalNode.insert(2, 1, 0, 0, 4, 4, seminar7, 0);
-
-        // create a search centered on (1,1) with radius 1
-        int nodesVisited = internalNode.search(0, 0, 0, 0, 4, 4, 3, 3, 0);
-
-        // all 3 seminars to be found
-        assertEquals(5, nodesVisited);
-
-        // search on the edge of the bounding box
-        int nodesVisitedOutsideBoundary = internalNode.search(2, 2, 0, 0, 4, 4,
-            1, 1, 0);
-        assertEquals(2, nodesVisitedOutsideBoundary);
     }
 
 
